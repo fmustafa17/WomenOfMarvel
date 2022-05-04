d04e8c2388ee336242eac914dbc80c5f05ab00c1
Farhana Mustafa <farhana_mustafa@intuit.com>
2022-05-03 21:03:10 -1000

    add details in vc unit tests

--- WomenOfMarvelTests/View/WomenOfMarvelViewControllerTests.swift
+++ WomenOfMarvelTests/View/WomenOfMarvelViewControllerTests.swift
@@ -11,10 +11,21 @@ import XCTest
 class WomenOfMarvelViewControllerTests: XCTestCase {
     // sut stands for system under test
     var sut: WomenOfMarvelViewController!
+
+    var mockTitleLabel: UILabel!
+    var mockDescriptionLabel: UILabel!
    
     override func setUp() {
         super.setUp()
         sut = WomenOfMarvelViewController()
+        sut.viewModel = WomenOfMarvelViewModel()
+
+        // Initialize the IBOutlets
+        mockTitleLabel = UILabel()
+        sut.comicBookTitle = mockTitleLabel
+
+        mockDescriptionLabel = UILabel()
+        sut.comicBookDescription = mockDescriptionLabel
     }
 
     override func tearDown() {
@@ -22,6 +33,21 @@ class WomenOfMarvelViewControllerTests: XCTestCase {
         sut = nil
     }
 
-    
+    /*
+    func testLabelsHaveTheCorrectText() {
+        // given
+        let mockResults = Result(id: 1590, digitalID: 0, title: "Women of Marvel", issueNumber: 0, variantDescription: "", resultDescription: "", modified: "", isbn: "", upc: "", diamondCode: "", ean: "", issn: "", format: "", pageCount: 0, textObjects: [], resourceURI: "", urls: [], series: Series(resourceURI: "", name: ""), variants: [], collections: [], collectedIssues: [], dates: [], prices: [], thumbnail: Thumbnail(path: "", thumbnailExtension: ""), images: [], creators: Creators(available: 0, collectionURI: "", items: [], returned: 0), characters: Characters(available: 0, collectionURI: "", items: [], returned: 0), stories: Stories(available: 0, collectionURI: "", items: [], returned: 0), events: Characters(available: 0, collectionURI: "", items: [], returned: 0))
+
+        let mockData = DataClass(offset: 0, limit: 0, total: 0, count: 0, results: [mockResults])
+
+        sut.viewModel.comicDetails = WomenOfMarvel(code: 200, status: "OK", copyright: "", attributionText: "", attributionHTML: "", etag: "", data: mockData)
+
+        // when
+        sut.viewDidLoad()
+
+        // then
+        XCTAssertEqual(sut.comicBookTitle.text, "Women of Marvel")
+    }
+    */
 
 }
